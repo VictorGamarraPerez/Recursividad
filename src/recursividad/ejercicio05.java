70c13173ced763f026fe256ded4480d7206c91d7
Coms By ASUS <Coms By ASUS@DESKTOP-RATC0JV>
2022-10-25 17:38:34 -0500

    Carga Inicial

---
+++ src/recursividad/ejercicio05.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package recursividad;
+
+/**
+ *
+ * @author 
+ */
+public class ejercicio05 {
+    
+ public static void main(String[] args) {
+       //numero de fibonacci
+    int num = 11;
+
+    System.out.println("Fibonacci de "+num+" es "+fibonacci1(num));
+    System.out.println("Fibonacci de "+num+" es "+fibonacci2(num));
+    System.out.println("Fibonacci de "+num+" es "+fibonacci3(num));
+}
+//solución 1
+static int fibonacci1(int n)
+{
+    if (n>1)  return fibonacci1(n-1) + fibonacci1(n-2);  //función recursiva
+    else if (n==1)
+        return 1;
+    else
+        return 0;
+}
+//solución 2
+static int fibonacci2(int n)
+{
+    if (n>1)
+    return fibonacci2(n-1) + fibonacci2(n-2);  //función recursiva
+    else return n;
+}
+//solución 3
+static int fibonacci3(int n)
+{
+    if (n<2)
+    return n;
+    else return fibonacci3(n-1) + fibonacci3(n-2);  //función recursiva
+    }   
+}
---
+++ src/recursividad/ejercicio06.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package recursividad;
+
+/**
+ *
+ * @author 
+ */
+public class ejercicio06 {
+    
+ public static void main(String[] args) {
+        int base=2;
+    int exp=4;
+    System.out.println(base+" elevado a "+exp+" = "+poten(base, exp));
+}
+//método que devuelve base elevado a exp
+//ejemplo: base = 2 y exp = 4 -> 16
+static int poten(int base, int exp)
+{
+    if(exp==0) return 1; // cualquier número elevado a cero es 1
+    else if(exp==1) return base;
+    else return base * poten(base,exp-1);
+    }  
+}
